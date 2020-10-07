e8a3b5a6f5c983d66b6ef4b7d660ee988cfdccf4
trmaxa2820 <60822358+trmaxa2820@users.noreply.github.com>
2020-10-07 22:51:45 +0300

    New spawner v0.1
    create spawner

---
+++ Assets/Scripts/CollapsingObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapsingObject : MonoBehaviour
+{
+    [SerializeField] private float _timeToDestroy;
+    private void OnEnable()
+    {
+        Destroy(gameObject, _timeToDestroy);
+    }
+}
---
+++ Assets/Scripts/PrimitiveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PrimitiveSpawner : MonoBehaviour
+{
+    [SerializeField] private CollapsingObject _collapsingObject;
+    [SerializeField] private Transform[] _transforms;
+    [SerializeField] private float _timeBetweenSpawn;
+
+    private Vector3[] _spawnPoints;
+    private Coroutine _spawnCoroutine;
+
+    private void Awake()
+    {
+        _spawnPoints = new Vector3[_transforms.Count()];
+        for(int i = 0; i < _transforms.Count(); i++)
+        {
+            _spawnPoints[i] = _transforms[i].position;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _spawnCoroutine = StartCoroutine(SpawnObject());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(_spawnCoroutine);
+    }
+    private IEnumerator SpawnObject()
+    {
+        var waitTime = new WaitForSeconds(_timeBetweenSpawn);
+        int i = 0;
+        while (true)
+        {
+            int countSpawnPoint = _transforms.Count() - 1;
+            Instantiate(_collapsingObject, _spawnPoints[i], Quaternion.identity);
+            i = i == countSpawnPoint ? 0 : i + 1;
+            yield return waitTime;
+        }
+    }
+}
